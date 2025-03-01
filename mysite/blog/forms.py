278831f658bc726fea78a706650adc157ff9aa1b
user <samsungcamera284@gmail.com>
2025-03-01 11:01:09 +0200

    initial

---
+++ mysite/blog/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+
+class CommentForm(forms.Form):
+    name = forms.CharField(max_length=25, widget=forms.TextInput(attrs={'class': 'form-control',
+                                                                        'id': 'nameInput'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control',
+                                                           'id': 'exampleInpuyEmail',
+                                                           'aria-descrebedby': 'emailHelp'}))
+
+    comment = forms.CharField(required=False, widget=forms.Textarea(attrs={'class': 'form-control',
+                                                                         'id':'comments', 'rows':'3'}))
+
+
+
+class EmailPostForm(forms.Form):
+    name = forms.CharField(max_length=25, widget=forms.TextInput(attrs={'class': 'form-control',
+                                                                         'id': 'nameInput'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control',
+                                    'id':'exampleInpuyEmail',
+                                    'aria-descrebedby': 'emailHelp'}))
+    to = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control',
+                                                        'id': 'emailTo'}))
+    comment = forms.CharField(required=False, widget=forms.Textarea(attrs={'class': 'form-control',
+                                                                         'id':'comments', 'rows':'3'}))
